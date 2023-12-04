4a555e03da44c365bb9f24bb5fce22b2f845578e
Ikdek <jules.pauchet1g1@gmail.com>
2023-12-04 12:23:08 +0100

    A quick initiation

---
+++ JeuWeb/script.js
@@ -0,0 +1,13 @@
+function myGame(playerName1,playerName2){
+    var hp1 = 100;
+    var hp2 = 100;
+    var round = 0
+    while (hp1 != 0 || hp2 != 0){
+        round += 1
+        turn(round)
+    }
+};
+
+function turn(round){
+
+}
---
+++ Jour 1/script.js
@@ -0,0 +1,8 @@
+var notes = [14,12,15,14,9,24,13,51,14,17,12]
+function bulletin(arg){
+    for (var i of notes){
+        console.log(i)
+    }
+}
+bulletin(notes)
+
