250014db8c47bfd6b0df433c505471a295d757c2
Jules Pauchet <jules.pauchet1g1@gmail.com>
2023-12-06 04:43:38 +0100

    J'arrive au bout

--- JeuWeb/script.js
+++ JeuWeb/script.js
@@ -1,3 +1,15 @@
+var shopButton1ClickListener = function () {
+    displayShopButtons("player1");
+};
+
+var shopButton2ClickListener = function () {
+    displayShopButtons("player2");
+};
+
+var exitButtonClickListener = function () {
+    exitShopMode();
+};
+
 window.onload = function () {
 
     var displayNamePlayer1, displayNamePlayer2;
@@ -8,16 +20,18 @@ window.onload = function () {
     var action1, action2;
     var inv1, inv2;
     var side, shopButtons1, shopButtons2;
-
+    var player1Def, player2Def; 
+    var playerDpsRate1 = 1; 
+    var playerDpsRate2 = 1;
     var potionCost = 30;
-    var bouclierCost = 50;
-
-//  ██████╗ ██╗   ██╗███████╗██████╗ ██╗   ██╗███████╗███████╗██╗     ███████╗ ██████╗████████╗ ██████╗ ██████╗ 
-// ██╔═══██╗██║   ██║██╔════╝██╔══██╗╚██╗ ██╔╝██╔════╝██╔════╝██║     ██╔════╝██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗
-// ██║   ██║██║   ██║█████╗  ██████╔╝ ╚████╔╝ ███████╗█████╗  ██║     █████╗  ██║        ██║   ██║   ██║██████╔╝
-// ██║▄▄ ██║██║   ██║██╔══╝  ██╔══██╗  ╚██╔╝  ╚════██║██╔══╝  ██║     ██╔══╝  ██║        ██║   ██║   ██║██╔══██╗
-// ╚██████╔╝╚██████╔╝███████╗██║  ██║   ██║   ███████║███████╗███████╗███████╗╚██████╗   ██║   ╚██████╔╝██║  ██║
-//  ╚══▀▀═╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚══════╝╚══════╝╚══════╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+    var epeeCost = 50;
+    
+    //  ██████╗ ██╗   ██╗███████╗██████╗ ██╗   ██╗███████╗███████╗██╗     ███████╗ ██████╗████████╗ ██████╗ ██████╗ 
+    // ██╔═══██╗██║   ██║██╔════╝██╔══██╗╚██╗ ██╔╝██╔════╝██╔════╝██║     ██╔════╝██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗
+    // ██║   ██║██║   ██║█████╗  ██████╔╝ ╚████╔╝ ███████╗█████╗  ██║     █████╗  ██║        ██║   ██║   ██║██████╔╝
+    // ██║▄▄ ██║██║   ██║██╔══╝  ██╔══██╗  ╚██╔╝  ╚════██║██╔══╝  ██║     ██╔══╝  ██║        ██║   ██║   ██║██╔══██╗
+    // ╚██████╔╝╚██████╔╝███████╗██║  ██║   ██║   ███████║███████╗███████╗███████╗╚██████╗   ██║   ╚██████╔╝██║  ██║
+    //  ╚══▀▀═╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚══════╝╚══════╝╚══════╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
 
     displayNamePlayer1 = document.querySelector("#playerName1");
     displayNamePlayer2 = document.querySelector("#playerName2");
@@ -25,6 +39,7 @@ window.onload = function () {
     start = document.querySelector("#start");
     title = document.querySelector("#title");
     status = document.querySelector(".player");
+
 
     hpPlayer1 = 100;
     hpPlayer2 = 100;
@@ -42,8 +57,9 @@ window.onload = function () {
     action1 = document.querySelector("#action1");
     action2 = document.querySelector("#action2");
 
-    inv1 = { ["Potions"]: 0, ["Bouclier"]: 0 };
-    inv2 = { ["Potions"]: 0, ["Bouclier"]: 0 };
+    inv1 = { "Potions": 1, "Epee": 1 };
+    inv2 = { "Potions": 1, "Epee": 1 };
+
 
     side = true;
     shopButtons1 = document.querySelector("#shopButtons1");
@@ -52,26 +68,32 @@ window.onload = function () {
     shopButtons1.style.display = "none";
     shopButtons2.style.display = "none";
 
-    // Masquer tous les éléments sauf start et title au chargement de la page
     status.style.display = "none";
     action1.style.display = "none";
     action2.style.display = "none";
 
     start.addEventListener("click", myGameInit);
 
-//  █████╗  ██████╗████████╗██╗ ██████╗ ███╗   ██╗███████╗
-// ██╔══██╗██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝
-// ███████║██║        ██║   ██║██║   ██║██╔██╗ ██║███████╗
-// ██╔══██║██║        ██║   ██║██║   ██║██║╚██╗██║╚════██║
-// ██║  ██║╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║███████║
-// ╚═╝  ╚═╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
-                                                       
-                                                                                                             
+    //  █████╗  ██████╗████████╗██╗ ██████╗ ███╗   ██╗███████╗
+    // ██╔══██╗██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝
+    // ███████║██║        ██║   ██║██║   ██║██╔██╗ ██║███████╗
+    // ██╔══██║██║        ██║   ██║██║   ██║██║╚██╗██║╚════██║
+    // ██║  ██║╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║███████║
+    // ╚═╝  ╚═╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+    var model1 = document.getElementById("player1Model");
+    var model2 = document.getElementById("player2Model");
+
     var attack1Button = document.querySelector("#attack1");
     attack1Button.addEventListener("click", attackPlayer1);
 
     var attack2Button = document.querySelector("#attack2");
     attack2Button.addEventListener("click", attackPlayer2);
+
+    var defend1Button = document.querySelector("#defend1");
+    defend1Button.addEventListener("click", defendPlayer1);
+
+    var defend2Button = document.querySelector("#defend2");
+    defend2Button.addEventListener("click", defendPlayer2);
 
     var shopButton1 = document.querySelector("#shop1");
     shopButton1.addEventListener("click", function () {
@@ -83,29 +105,35 @@ window.onload = function () {
         displayShopButtons("player2");
     });
 
-    var exitButton = document.querySelector("#exit");
+
+    var itemButton1 = document.querySelector("#items1");
+    itemButton1.addEventListener("click", function () {
+        displayItemsButtons("player1");
+    });
+
+    var itemButton2 = document.querySelector("#items2");
+    itemButton2.addEventListener("click", function () {
+        displayItemsButtons("player2");
+    });
+
+    var exitButton = document.querySelector(".exitShop");
     exitButton.addEventListener("click", function () {
         exitShopMode();
     });
-
-    var itemsButton1 = document.querySelector("#items1");
-    itemsButton1.addEventListener("click", function () {
-        displayItemsMenu("player1");
-    });
-
-    var itemsButton2 = document.querySelector("#items2");
-    itemsButton2.addEventListener("click", function () {
-        displayItemsMenu("player2");
+    var exitItems = document.querySelector(".exitItems");
+    exitItems.addEventListener("click", function () {
+        exitShopMode();
     });
 
 
-// ███████╗██╗   ██╗███╗   ██╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗███████╗
-// ██╔════╝██║   ██║████╗  ██║██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝
-// █████╗  ██║   ██║██╔██╗ ██║██║        ██║   ██║██║   ██║██╔██╗ ██║███████╗
-// ██╔══╝  ██║   ██║██║╚██╗██║██║        ██║   ██║██║   ██║██║╚██╗██║╚════██║
-// ██║     ╚██████╔╝██║ ╚████║╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║███████║
-// ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
-                                                                          
+
+    // ███████╗██╗   ██╗███╗   ██╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗███████╗
+    // ██╔════╝██║   ██║████╗  ██║██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝
+    // █████╗  ██║   ██║██╔██╗ ██║██║        ██║   ██║██║   ██║██╔██╗ ██║███████╗
+    // ██╔══╝  ██║   ██║██║╚██╗██║██║        ██║   ██║██║   ██║██║╚██╗██║╚════██║
+    // ██║     ╚██████╔╝██║ ╚████║╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║███████║
+    // ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+
 
 
 
@@ -125,39 +153,112 @@ window.onload = function () {
         myRounds();
     }
 
+
+    function randomAtt() {
+        return Math.floor(Math.random() * (20 - 10 + 1)) + 10;
+    }
+
+
     function myRounds() {
         if (side === true) {
             action2.style.display = "none";
             action1.style.display = "flex";
+            player1Def = false;
+            playerDpsRate2 = 1;
+            model2.classList.remove("hited")
         } else {
             action1.style.display = "none";
             action2.style.display = "flex";
             round += 1;
             displayRound.innerText = "round " + round;
+            player2Def = false;
+            playerDpsRate1 = 1;
+            model1.classList.remove("hited")
         }
+    }
+
+    function updateButtonsDisplay(player) {
+        var actionContainer = document.querySelector("#action" + (player === "player1" ? "1" : "2"));
+        var inventory = document.querySelector("#inventoryPlayer" + (player === "player1" ? "1" : "2"));
+
+        if (actionContainer && inventory) {
+            if (player === "player1") {
+                action1.style.display = "flex";
+            } else {
+                action2.style.display = "flex";
+            }
+            inventory.style.display = "none";
+        }
+    }
+
+    function removeShopEventListeners() {
+        shopButton1.removeEventListener("click", shopButton1ClickListener);
+        shopButton2.removeEventListener("click", shopButton2ClickListener);
+        exitButton.removeEventListener("click", exitButtonClickListener);
     }
 
     function attackPlayer1() {
         if (side === true) {
-            hpPlayer2 -= 20;
-            displayHpPlayer2.innerText = hpPlayer2 + " / 100❤️";
-            side = false;
-            coinsPlayer1 += 20;
-            displaycoinsPlayer1.innerText = coinsPlayer1 + "🪙";
-            myRounds();
+            if (player2Def == true) {
+                side = false;
+                coinsPlayer1 += 20;
+                displaycoinsPlayer1.innerText = coinsPlayer1 + "🪙";
+                model2.classList.add("hitedShield")
+                myRounds();
+            } else {
+                hpPlayer2 -= randomAtt()*playerDpsRate1;
+                displayHpPlayer2.innerText = hpPlayer2 + " / 100❤️";
+                side = false;
+                model2.classList.add("hited")
+                checkGameOver();
+            }
+
         }
     }
 
     function attackPlayer2() {
         if (side === false) {
-            hpPlayer1 -= 20;
-            displayHpPlayer1.innerText = hpPlayer1 + " / 100❤️";
-            side = true;
-            coinsPlayer2 += 20;
-            displaycoinsPlayer2.innerText = coinsPlayer2 + "🪙";
+            if (player1Def == true) {
+                side = true;
+                coinsPlayer2 += 20;
+                displaycoinsPlayer2.innerText = coinsPlayer2 + "🪙";
+                model1.classList.add("hitedShield")
+                myRounds();
+            } else {
+                hpPlayer1 -= randomAtt()*playerDpsRate2;
+                displayHpPlayer1.innerText = hpPlayer1 + " / 100❤️";
+                side = true;
+                model1.classList.add("hited")
+                checkGameOver();
+            }
+
+        }
+    }
+
+    function defendPlayer1() {
+        if (side === true) {
+            player1Def = true;
+            coinsPlayer1 += 10;
+            displaycoinsPlayer1.innerText = coinsPlayer1 + "🪙";
+            side = false;
+            model1.classList.add("hitedShield")
             myRounds();
         }
     }
+
+    function defendPlayer2() {
+        if (side === false) {
+            player2Def = true;
+            coinsPlayer2 += 10;
+            displaycoinsPlayer2.innerText = coinsPlayer2 + "🪙";
+            side = true;
+            model1.classList.add("hitedShield")
+            myRounds();
+        }
+    }
+
+
+
 
     function displayShopButtons(player) {
         var actionContainer = document.querySelector("#action" + (player === "player1" ? "1" : "2"));
@@ -167,15 +268,15 @@ window.onload = function () {
             actionContainer.style.display = "none";
             shopButtons.style.display = "flex";
             var potionButton = document.querySelector("#potions" + (player === "player1" ? "1" : "2"));
-            var bouclierButton = document.querySelector("#bouclier" + (player === "player1" ? "1" : "2"));
+            var epeeButton = document.querySelector("#epee" + (player === "player1" ? "1" : "2"));
 
-            if (potionButton && bouclierButton) {
+            if (potionButton && epeeButton) {
                 potionButton.addEventListener("click", function () {
-                    buyItem(player, "potion");
+                    buyItem(player, "Potions");
                 });
 
-                bouclierButton.addEventListener("click", function () {
-                    buyItem(player, "bouclier");
+                epeeButton.addEventListener("click", function () {
+                    buyItem(player, "Epee");
                 });
             }
         }
@@ -183,43 +284,36 @@ window.onload = function () {
 
     function buyItem(player, item) {
         var currentPlayerCoins = (player === "player1" ? coinsPlayer1 : coinsPlayer2);
-
-        if (currentPlayerCoins >= potionCost && item === "potion") {
+        var inventory = (player === "player1" ? inv1 : inv2);
+        if (currentPlayerCoins >= potionCost && item === "Potions") {
+            currentPlayerCoins -= potionCost;
             if (player === "player1") {
-                coinsPlayer1 -= potionCost;
-                displaycoinsPlayer1.innerText = coinsPlayer1 + "🪙";
-                inv1["Potions"] += 1;
-                document.getElementById("potions1Count").innerText = inv1["Potions"];
+                displaycoinsPlayer1.innerText = currentPlayerCoins + "🪙";
             } else {
-                coinsPlayer2 -= potionCost;
-                displaycoinsPlayer2.innerText = coinsPlayer2 + "🪙";
-                inv2["Potions"] += 1;
-                document.getElementById("potions2Count").innerText = inv2["Potions"];
+                displaycoinsPlayer2.innerText = currentPlayerCoins + "🪙";
             }
-
-            console.log("Potion achetée!");
-        } else if (currentPlayerCoins >= bouclierCost && item === "bouclier") {
+            inventory["Potions"] += 1;
+            console.log("Potion achetée! " + player);
+        } else if (currentPlayerCoins >= epeeCost && item === "Epee") {
+            currentPlayerCoins -= epeeCost;
             if (player === "player1") {
-                coinsPlayer1 -= bouclierCost;
-                displaycoinsPlayer1.innerText = coinsPlayer1 + "🪙";
-                inv1["Bouclier"] += 1;
-                document.getElementById("bouclier1Count").innerText = inv1["Bouclier"];
+                displaycoinsPlayer1.innerText = currentPlayerCoins + "🪙";
             } else {
-                coinsPlayer2 -= bouclierCost;
-                displaycoinsPlayer2.innerText = coinsPlayer2 + "🪙";
-                inv2["Bouclier"] += 1;
-                document.getElementById("bouclier2Count").innerText = inv2["Bouclier"];
+                displaycoinsPlayer2.innerText = currentPlayerCoins + "🪙";
             }
-
-            console.log("Bouclier acheté!");
+            inventory["Epee"] += 1;
+            console.log("Epée achetée! " + player);
         } else {
-            console.log("Pas assez de pièces de monnaie.");
+            alert("Pas assez de pièces de monnaie.");
         }
 
         exitShopMode();
     }
 
+
     function exitShopMode() {
+        removeShopEventListeners();
+    
         action1.style.display = (side ? "flex" : "none");
         action2.style.display = (!side ? "flex" : "none");
         shopButtons1.style.display = "none";
@@ -228,18 +322,90 @@ window.onload = function () {
         document.getElementById("inventoryPlayer2").style.display = "none";
     }
 
-    function displayItemsMenu(player) {
-        var inventory = document.getElementById("inventory" + player);
-        var itemButtons = document.getElementsByClassName("itemButton");
+    function displayItemsButtons(player) {
+        var actionContainer = document.querySelector("#action" + (player === "player1" ? "1" : "2"));
+        var inventory = document.querySelector("#inventoryPlayer" + (player === "player1" ? "1" : "2"));
 
-        if (inventory && itemButtons.length > 0) {
-            shopButtons1.style.display = "none";
-            shopButtons2.style.display = "none";
+        if (actionContainer && inventory) {
+            actionContainer.style.display = "none";
             inventory.style.display = "flex";
+            var potionButton = document.querySelector("#potionsButton" + (player === "player1" ? "1" : "2"));
+            var epeeButton = document.querySelector("#epeeButton" + (player === "player1" ? "1" : "2"));
+            if (player == "player1") {
+                potionButton.innerText = `Potions : ${inv1["Potions"]}`
+                epeeButton.innerText = `Epée : ${inv1["Epee"]}`
+            } else {
+                potionButton.innerText = `Potions : ${inv2["Potions"]}`
+                epeeButton.innerText = `Epée : ${inv2["Epee"]}`
+            }
 
-            for (var i = 0; i < itemButtons.length; i++) {
-                itemButtons[i].style.display = "block";
+            if (potionButton && epeeButton) {
+                potionButton.addEventListener("click", function () {
+                    useItem(player, "potion");
+                });
+
+                epeeButton.addEventListener("click", function () {
+                    useItem(player, "epee");
+                });
             }
         }
+    }
+
+    function useItem(player, item) {
+        if (item === "epee") {
+            if (player === "player1") {
+                playerDpsRate1 = 2;
+                inv1["Epee"] -= 1 
+            } else {
+                playerDpsRate2 = 2;
+                inv2["Epee"] -= 1 
+            }
+        }else if (item == "potion"){
+            if (player === "player1") {
+                if (hpPlayer1 <= 80){
+                    hpPlayer1 += 20;
+                displayHpPlayer1.innerText = hpPlayer1 + " / 100❤️";
+                inv1["Potions"] -= 1;
+                }else{
+                    hpPlayer1 = 100;
+                    displayHpPlayer1.innerText = hpPlayer1 + " / 100❤️";
+                inv1["Potions"] -= 1;
+                }
+                
+            } else {
+                if (hpPlayer2 <= 80){
+                    hpPlayer2 += 20;
+                displayHpPlayer2.innerText = hpPlayer1 + " / 100❤️";
+                inv2["Potions"] -= 1;
+                }else{
+                    hpPlayer2 = 100;
+                    displayHpPlayer2.innerText = hpPlayer2 + " / 100❤️";
+                inv2["Potions"] -= 1;
+                }
+            }
+            }
+        updateButtonsDisplay(player);
+    }
+    function checkGameOver() {
+        if (hpPlayer1 <= 0 || hpPlayer2 <= 0) {
+            endGame();
+        } else {
+            myRounds();
+        }
+    }
+
+    function endGame() {
+        var deathMessage = document.querySelector("#deadTitle");
+
+        if (hpPlayer1 <= 0 && hpPlayer2 <= 0) {
+            deathMessage.innerText = "Match nul !";
+        } else if (hpPlayer1 <= 0) {
+            deathMessage.innerText = `${displayNamePlayer1.innerText} est mort ...`;
+        } else {
+            deathMessage.innerText = `${displayNamePlayer2.innerText} est mort ...`;
+        }
+
+        status.style.display = "none";
+        deathMessage.style.display = "block";
     }
 };
